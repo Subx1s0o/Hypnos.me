556841cf1e41e812d67ad1360b27329da26c5aa6
Subx1s0o <nazarsabardin@gmail.com>
2024-10-18 01:43:50 +0300

    feat: add font sizes

--- src/components/common/HomeHeader.tsx
+++ src/components/common/HomeHeader.tsx
@@ -4,15 +4,15 @@ import Icon from '../features/Icon'
 
 export default function HomeHeader() {
   return (
-    <header className='hidden md:visible'>
+    <header className='hidden md:block'>
       <div className='px-10'>
         <ul>
           <li>
             <div className='flex'>
               <Icon
                 id='icon-profile'
-                w={18}
-                h={18}
+                w={24}
+                h={24}
               />
               <Link to='/profile'>Profile</Link>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,64 @@ export default {
         black: '#1B1716',
         'light-grey': '#F3F1EF',
         cream: '#E5DCD4'
+      },
+      fontSize: {
+        xxl: [
+          '104px',
+          {
+            fontWeight: 300,
+            lineHeight: '126px'
+          }
+        ],
+        xl: [
+          '64px',
+          {
+            fontWeight: 500,
+            lineHeight: '77px'
+          }
+        ],
+        lg: [
+          '56px',
+          {
+            fontWeight: 300,
+            lineHeight: '68px'
+          }
+        ],
+        md: [
+          '40px',
+          {
+            fontWeight: 700,
+            lineHeight: '48px'
+          }
+        ],
+        smd: [
+          '16px',
+          {
+            fontWeight: 700,
+            lineHeight: '22px'
+          }
+        ],
+        base: [
+          '18px',
+          {
+            fontWeight: 500,
+            lineHeight: '22px'
+          }
+        ],
+        sm: [
+          '14px',
+          {
+            fontWeight: 500,
+            lineHeight: '21px'
+          }
+        ],
+        xs: [
+          '12px',
+          {
+            fontWeight: 500,
+            lineHeight: '16px'
+          }
+        ]
       }
     }
   },
