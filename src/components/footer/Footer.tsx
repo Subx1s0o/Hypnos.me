6cfb4fb80d317273484de20d6e6cdeb286d3b034
Vita <nikaangel295@gmail.com>
2024-11-06 13:22:55 +0000

    add

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -20,11 +20,18 @@ const Footer: React.FC = () => {
              <div className="text-center bg-black text-white">
                 <h3 className="">Stay in touch</h3>
                 <p className="">Sign up for our newsletter to receive 500 points on your first order, which you can use immediately.</p>
-                <form className="flex flex-col items-center space-y-4">
-                    <input className="p-2 border border-gray-300 rounded-md" placeholder="Email" />
-                    <button className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" type="submit" >Send</button>
-                    <label><input className="flex items-center space-x-2" type="chekbox" />I agree with the site policy</label>
-                </form>
+                <form 
+    className="flex flex-col items-center space-y-4 bg-white p-4 border border-gray-200 rounded-md" 
+    // style="width: 396px; height: 166px; position: absolute; top: 40px; left: 498px;"
+>
+    <input className="p-2 border border-gray-300 rounded-md w-full" placeholder="Email" />
+    <button className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 w-full" type="submit">Send</button>
+    <label className="flex items-center space-x-2">
+        <input type="checkbox" className="mr-2" />
+        <span>I agree with the site policy</span>
+    </label>
+</form>
+
                 
             </div>
             
