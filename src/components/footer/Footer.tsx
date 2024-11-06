bc02d3e9ee912c58a145cc8b8a123134c8c44930
Vita <nikaangel295@gmail.com>
2024-11-06 13:42:47 +0000

    add

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,9 +9,7 @@ import FooterContactList from "./components/FooterContactList";
 const Footer: React.FC = () => {
     return (
         <footer className="bg-gray-900">
-
-           
-            <h3 className='text-md-thin font-cormorant text-center'>HYPNOS</h3>
+            <h3 className='text-md font-cormorant text-center'>HYPNOS</h3>
             <div className="flex gap-4 justify-around items-start p-4">
             <FooterInfoList infoList={siteMapList} title='Site map' />
             <FooterInfoList infoList={serviceList} title='Service' /> 
@@ -19,15 +17,15 @@ const Footer: React.FC = () => {
             <FooterInfoList infoList={loewHelpsList} title='Loe info' /></div>
              <div className="text-center bg-black text-white w-[1392px] h-[291px]">
                 <h3 className="">Stay in touch</h3>
-                <p className="">Sign up for our newsletter to receive 500 points on your first order, which you can use immediately.</p>
+                <p className="text-center max-w-md">Sign up for our newsletter to receive 500 points on your first order, which you can use immediately.</p>
                 <form 
-    className="flex flex-col items-center space-y-4 bg-white p-4 border border-gray-200 rounded-md" 
+    className="flex flex-col items-center space-y-4" 
     // style="width: 396px; height: 166px; position: absolute; top: 40px; left: 498px;"
 >
-    <input className="p-2 border border-gray-300 rounded-md w-full" placeholder="Email" />
-    <button className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 w-full" type="submit">Send</button>
+    <input className="w-[396px] h-[40px] p-2 border border-gray-300 rounded-md" placeholder="Email" />
+    <button className="w-[396px] h-[40px] p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" type="submit">Send</button>
     <label className="flex items-center space-x-2">
-        <input type="checkbox" className="mr-2" />
+        <input className="w-4 h-4" type="checkbox" />
         <span>I agree with the site policy</span>
     </label>
 </form>
