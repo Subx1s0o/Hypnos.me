6083c9b9a418f86e5a4b376c72980f14343ae6e5
Vita <nikaangel295@gmail.com>
2024-11-07 12:00:41 +0000

    add

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,19 +14,23 @@ const Footer: React.FC = () => {
             <FooterInfoList infoList={serviceList} title='Services' /> 
             <FooterContactList />
             </div>
-             <div className="text-center bg-black text-white w-[1392px] h-[291px]">
-                <h3 className="">Stay in touch</h3>
-                <p className="text-center max-w-md mx-auto">Sign up for our newsletter to receive 500 points on your first order, which you can use immediately.</p>
-          <form className="flex flex-col items-center space-y-4">
-            <input className="w-[396px] h-[40px] p-2 border border-gray-300 rounded-md" type="text" 
-               placeholder="Email" />
-               <button className="w-[396px] h-[40px] p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" type="submit">Send</button>
-                <label className="flex items-center space-x-2">
-             <input className="w-4 h-4" type="checkbox" />
-               <span>I agree with the site policy</span>
-               </label>
-         </form>    
-            </div>
+            <div className="text-center bg-black text-white w-[1392px] h-[291px] flex justify-between items-center px-8">
+    <div className="">
+        <p className="text-left max-w-[290px] h-[84px] top-[727px] left-[691px]">
+            Be the first to know about promotions, raffles and auctions for exclusive jewelry that we conduct only among the sites audience.
+        </p>
+    </div>
+
+    <form className="flex flex-col items-center space-y-4 w-[50%]">
+        <input className="w-[396px] h-[40px] p-2 border border-gray-300 rounded-md" type="text" placeholder="Email" />
+        <button className="w-[396px] h-[40px] p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" type="submit">Send</button>
+        <label className="flex items-center space-x-2">
+            <input className="w-4 h-4" type="checkbox" />
+            <span>I agree with the site policy</span>
+        </label>
+    </form>
+</div>
+
             
     </footer>
         
