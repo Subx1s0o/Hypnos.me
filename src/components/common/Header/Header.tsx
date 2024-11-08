f581af91cc0a1ea2621e80c9bca2f55c75a04b24
Subx1s0o <nazarsabardin@gmail.com>
2024-11-08 23:03:54 +0200

    refactor: refactor header

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom'
+
+import Icon from '@/components/ui/Icon'
+
+import HeaderNavigation from '../Navigations/HeaderNavigation'
+
+export default function HomeHeader() {
+  return (
+    <header className='px-10'>
+      <div className='border-b-grey-200 flex items-center justify-between border-b py-3'>
+        <Link
+          to='/'
+          className=''>
+          <Icon
+            id='icon-logo'
+            className='text-brown'
+            w={150}
+            h={35}
+          />
+        </Link>
+        <HeaderNavigation />
+        <div>
+          <address className='relative flex gap-6 text-xs not-italic'>
+            <a
+              href='tel:12155550199'
+              className='text-xs-heavy'>
+              +1 215-555-0199
+            </a>
+
+            <a
+              href='https://maps.app.goo.gl/B9Yav26Ua82HB1vS9'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-xs-heavy'>
+              123 Walnut Street
+            </a>
+          </address>
+        </div>
+      </div>
+    </header>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,10 @@ export default {
         cormorant: ['Cormorant Garamond', 'serif']
       },
       colors: {
-        grey: '#4E453E',
+        grey: {
+          '200': '#E8E6E5',
+          '400': '#4E453E'
+        },
         black: '#1B1716',
         'light-grey': '#F3F1EF',
         cream: '#E5DCD4',
@@ -79,6 +82,13 @@ export default {
           {
             fontWeight: 500,
             lineHeight: '21px'
+          }
+        ],
+        'xs-heavy': [
+          '12px',
+          {
+            fontWeight: 600,
+            lineHeight: '16px'
           }
         ],
         xs: [
