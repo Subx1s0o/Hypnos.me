d74d68b6a21c989b405171a6f7ad69b9fb2caff6
Subx1s0o <nazarsabardin@gmail.com>
2024-11-16 23:35:20 +0200

    fix: rename component in Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Home from '@/components/common/Home/HeaderOrMenu/HeaderOrMenu'
+import HeaderOrMenu from '@/components/common/Home/HeaderOrMenu/HeaderOrMenu'
 import AboutSection from '@/components/sections/AboutSection/AboutSection'
 import HeroSection from '@/components/sections/HeroSection/HeroSection'
 
 export default function HomePage() {
   return (
     <>
-      <Home />
+      <HeaderOrMenu />
       <main>
         <HeroSection />
         <AboutSection />
