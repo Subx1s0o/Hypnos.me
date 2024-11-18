9a1b223045412b3d208c2ade35efd027a7f0f603
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2024-11-18 18:55:32 +0200

    fix svg-crystal

--- src/components/sections/AboutSection/AboutSection.tsx
+++ src/components/sections/AboutSection/AboutSection.tsx
@@ -1,15 +1,22 @@
+import Link from 'next/link'
+
 import Icon from '../../ui/Icon'
-import SvgCristall from './SvgCristall'
 
 export default function AboutSection() {
   return (
-    <section>
-      <SvgCristall />
+    <section className='relative overflow-hidden py-12 pb-32 sm:px-4 md:px-10'>
+      <Icon
+        className='absolute -top-16 left-1/2 -translate-x-1/2 scale-75 text-primary lg:top-10
+          lg:scale-110'
+        id='icon-crystal'
+        w={871}
+        h={682}
+      />
 
-      <div className='relative mx-auto px-4 sm:max-w-xl lg:max-w-4xl lg:px-11'>
+      <div className='mx-auto px-4 sm:max-w-xl lg:max-w-4xl lg:px-11'>
         <h2
-          className='py-12 font-cormorant text-2xl font-medium uppercase not-italic tracking-tighter
-            text-brown sm:text-center md:pb-28 md:text-4xl lg:text-6xl'>
+          className='mb-12 font-cormorant text-2xl font-medium uppercase not-italic tracking-tighter
+            text-brown sm:text-center md:pb-14 md:text-4xl lg:text-6xl'>
           SINCE 2012 WE HAVE BEEN CREATING EXCLUSIVE JEWELLERY TO ORDER,
           FULFILLING THE MOST DARING JEWELLERY DREAMS.
         </h2>
@@ -29,15 +36,17 @@ export default function AboutSection() {
               filled with jewellery that is close to your values and ideology.
             </p>
           </div>
-          <div className='col-start-3 -ml-12 md:ml-0'>
+          <Link
+            href='custom'
+            className='col-start-3 -ml-12 size-14 flex-none shrink-0 text-white md:ml-0'>
             <Icon
-              className='pointer-events-auto absolute rotate-45 rounded-full bg-brown p-5 text-white
-                transition-all hover:bg-red-800 lg:block'
+              className='shrink-0 rotate-45 rounded-full bg-brown p-5 transition-all hover:bg-red-800
+                lg:block'
               id='icon-arrow'
-              w={20}
-              h={20}
+              w={16}
+              h={16}
             />
-          </div>
+          </Link>
         </div>
       </div>
     </section>
