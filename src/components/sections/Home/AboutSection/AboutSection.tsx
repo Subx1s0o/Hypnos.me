8614e0f9cd881b5bd85e0128fa5851f5909413e1
Subx1s0o <nazarsabardin@gmail.com>
2024-11-21 14:43:19 +0200

    fix: fix styles

--- src/components/sections/Home/AboutSection/AboutSection.tsx
+++ src/components/sections/Home/AboutSection/AboutSection.tsx
@@ -1,26 +1,28 @@
 import Link from 'next/link'
 
-import Icon from '../../ui/Icon'
+import Icon from '@/components/ui/Icon'
 
 export default function AboutSection() {
   return (
-    <section className='relative flex justify-center overflow-hidden py-12 pb-32 px-4 md:px-10'>
+    <section className='relative flex justify-center overflow-hidden px-4 py-12 pb-32 md:px-10'>
       <Icon
-        className='absolute -top-16 left-[50.5%] -translate-x-1/2 scale-75 text-light-grey -z-10 lg:top-[12rem]
-          lg:scale-[1.55]'
+        className='absolute -top-16 left-[50.5%] -z-10 -translate-x-1/2 scale-75 text-light-grey
+          lg:top-48 lg:scale-[1.55]'
         id='icon-crystal'
         w={871}
         h={682}
       />
 
-      <div className='sm:max-w-xl lg:max-w-4xl'>
-        <h2 className='mb-12 font-cormorant text-2xl  text-brown sm:text-center md:mb-[124px] sm:text-md lg:text-lg'>
+      <div className='sm:max-w-xl lg:max-w-5xl'>
+        <h2
+          className='mb-12 font-cormorant text-2xl text-brown sm:text-center sm:text-md md:mb-[124px]
+            lg:text-md xl:text-lg'>
           SINCE 2012 WE HAVE BEEN CREATING EXCLUSIVE JEWELLERY TO ORDER,
           FULFILLING THE MOST DARING JEWELLERY DREAMS.
         </h2>
-        <div className='flex flex-col sm:grid grid-cols-2 gap-5'>
+        <div className='flex grid-cols-2 flex-col gap-5 md:grid'>
           <div className='col-start-2'>
-            <div className='flex flex-col gap-4 sm:gap-12 text-black mb-[48px] md:mb-[108px]'>
+            <div className='mb-[48px] flex flex-col gap-4 text-black sm:gap-12 md:mb-[108px]'>
               <p className='text-base'>
                 Our online shop features the works of talented Ukrainian
                 designers creating jewellery made of silver, gold, brass, steel
@@ -36,7 +38,7 @@ export default function AboutSection() {
             </div>
             <Link
               href='custom'
-              className='flex text-white justify-center sm:justify-start'>
+              className='flex justify-center text-white md:justify-start'>
               <Icon
                 className='shrink-0 rotate-45 rounded-full bg-brown p-5 transition-all hover:bg-red-800
                   lg:block'
