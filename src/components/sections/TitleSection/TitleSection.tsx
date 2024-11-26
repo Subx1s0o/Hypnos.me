faac47572de7e9a2824e258555ba5fe278f96fde
xd.faunredd <andrew.furevych@gmail.com>
2024-11-26 19:16:28 +0200

    add underline for nav list , remove diamond

--- src/components/sections/TitleSection/TitleSection.tsx
+++ src/components/sections/TitleSection/TitleSection.tsx
@@ -1,23 +1,13 @@
-import Icon from '@/components/ui/Icon'
-
 import NavigationList from './components/NavigationList'
 
 const TitleSection = () => {
   return (
-    <section className='overflow-hidden'>
+    <section>
       <div className='relative px-[24px] pt-[75px] md:px-[43px] md:pb-[27px] md:pt-[53px]'>
-        <Icon
-          className='absolute -top-1/2 left-[51%] z-[-1] -translate-x-1/2 scale-[0.5] text-[#ccc0b1]
-            text-opacity-[0.5] md:top-0 md:scale-[1.01]'
-          id='icon-crystal'
-          w={800}
-          h={680}
-        />
-
         <NavigationList />
         <h1
-          className='mx-auto mt-3 flex max-w-[327px] justify-center text-center font-cormorant
-            text-lg uppercase md:max-w-[510px] md:text-xxl'>
+          className='mx-auto max-w-[327px] text-center font-cormorant text-lg md:max-w-[510px]
+            md:text-xxl'>
           JEWELLERY ATELIER
         </h1>
       </div>
--- src/components/sections/TitleSection/components/NavigationList.tsx
+++ src/components/sections/TitleSection/components/NavigationList.tsx
@@ -2,33 +2,41 @@ import Link from 'next/link'
 
 const NavigationList = () => {
   return (
-    <nav className='hidden md:block'>
+    <nav className='mb:mb-3 hidden md:block'>
       <ul className='flex justify-center gap-slide-large font-manrope text-sm text-grey-400'>
-        <li>
+        <li className='relative'>
           <Link
             href={'#catalogue'}
-            className='duration-300 hover:text-gray-300'>
+            className='duration-300 after:absolute after:bottom-[-5px] after:left-1/2 after:h-[2px]
+              after:w-0 after:-translate-x-1/2 after:bg-grey-400 after:duration-300
+              hover:after:w-full'>
             Catalogue
           </Link>
         </li>
-        <li>
+        <li className='relative'>
           <Link
             href={'#products'}
-            className='duration-300 hover:text-gray-300'>
+            className='duration-300 after:absolute after:bottom-[-5px] after:left-1/2 after:h-[2px]
+              after:w-0 after:-translate-x-1/2 after:bg-grey-400 after:duration-300
+              hover:after:w-full'>
             New products
           </Link>
         </li>
-        <li>
+        <li className='relative'>
           <Link
             href={'#about'}
-            className='duration-300 hover:text-gray-300'>
+            className='duration-300 after:absolute after:bottom-[-5px] after:left-1/2 after:h-[2px]
+              after:w-0 after:-translate-x-1/2 after:bg-grey-400 after:duration-300
+              hover:after:w-full'>
             About us
           </Link>
         </li>
-        <li>
+        <li className='relative'>
           <Link
             href={'#contacts'}
-            className='duration-300 hover:text-gray-300'>
+            className='duration-300 after:absolute after:bottom-[-5px] after:left-1/2 after:h-[2px]
+              after:w-0 after:-translate-x-1/2 after:bg-grey-400 after:duration-300
+              hover:after:w-full'>
             Contacts
           </Link>
         </li>
