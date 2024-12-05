e86acb2f575e6d19423756ed6df2ac14d9a49d2a
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2024-12-05 06:23:01 +0200

    add ReviewCard

---
+++ src/components/sections/ReviewsSection/components/ReviewCard.tsx
@@ -0,0 +1,16 @@
+export default function ReviewCard() {
+    return(
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quaerat, architecto totam obcaecati recusandae rem at ipsum consequatur soluta iste.</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="292" height="33" viewBox="0 0 292 33" fill="none">
+  <path d="M0 1H86C87.1046 1 88 1.89543 88 3V31.3062C88 32.0674 88.8166 32.5495 89.4831 32.1818L145.549 1.24884C145.845 1.08561 146.177 1 146.515 1H292" stroke="#D9CABF" stroke-miterlimit="3.8637" stroke-linejoin="round"/>
+</svg>
+            <div>
+                <img src='/images/HeroSection/catalog.avif' alt="fotoslide" className='rounded-[64px] w-16 h-16'/>
+                <h3>Courtney Henry</h3>
+                <p>24.02.2022</p>
+            </div>
+        </div>
+    )
+
+}
