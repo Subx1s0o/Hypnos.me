a3f28608435d1077efcd7f7b553b85e935f3a536
Subx1s0o <nazarsabardin@gmail.com>
2024-12-19 21:56:46 +0200

    fix: lint fixes

--- src/components/sections/Home/ProductsSection/components/ProductItem.tsx
+++ src/components/sections/Home/ProductsSection/components/ProductItem.tsx
@@ -23,7 +23,7 @@ export default function ProductItem({ product }: ProductItemProps) {
   const finalPrice = discountPercent
     ? price - (price * discountPercent) / 100
     : price
-  console.log(finalPrice)
+
   return (
     <li className='aspect-square flex-1 pl-4 sm:flex-1/2 lg:flex-1/4'>
       <Link href={`products/${category}/${slug}`}>
