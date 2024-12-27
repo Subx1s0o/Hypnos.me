0abceed8d298f8050a9ca6e012caaf0d8369a66b
Vita <nikaangel295@gmail.com>
2024-12-27 18:14:28 +0000

    fix

--- src/components/footer/components/FooterContactList.tsx
+++ src/components/footer/components/FooterContactList.tsx
@@ -1,11 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 
 const FooterContactList = () => {
     return (
         <div className="bg-black text-white w-[330px] h-[400px] p-[15px]">
-            <h4 className="text-left text-[12px] font-semibold font-manrope leading-[16.39px] tracking-[0.01em] gap-8 mb-[32px]">Schedule</h4>
+            <h4 className="text-left text-[12px] font-semibold font-manrope leading-[16.39px] tracking-[0.01em] gap-8 mb-slide-large">Schedule</h4>
             <div className="flex gap-10">
                 <p>Mon—Fri</p>
                 <p>10:00 — 19:00</p>
