82182b5b9c8cebab871d3d556c5fa2e679ad0121
Vita <nikaangel295@gmail.com>
2024-12-27 18:52:29 +0000

    fix

--- src/components/footer/components/FooterContactList.tsx
+++ src/components/footer/components/FooterContactList.tsx
@@ -5,12 +5,12 @@
 const FooterContactList = () => {
     return (
         <div className="bg-black text-white w-[330px] h-[400px] p-[15px]">
-            <h4 className="text-left text-[12px] font-semibold font-manrope leading-[16.39px] tracking-[0.01em] gap-8 mb-slide-large">Schedule</h4>
-            <div className="flex gap-10">
+            <h4 className="text-left text-[12px] font-semibold font-manrope leading-[16.39px] tracking-[0.01em] gap-8 mb-slide-large ">Schedule</h4>
+            <div className="flex gap-16">
                 <p>Mon—Fri</p>
                 <p>10:00 — 19:00</p>
             </div>
-            <div className="flex gap-10">
+            <div className="flex gap-16">
                 <p>Sat—Sun</p>
                 <p>By appointment only</p>
             </div>
