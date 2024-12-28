26ad7dcc35c258c2063131904605db50ff2399b9
Vita <nikaangel295@gmail.com>
2024-12-28 15:06:29 +0000

    fix footer-section

--- src/components/footer/components/FooterSubscribeList.tsx
+++ src/components/footer/components/FooterSubscribeList.tsx
@@ -1,33 +1,3 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
-
-// const FooterContactList = () => {
-//     return (
-//         <div className="bg-black text-white  h-[400px] p-[15px]">
-//             <h4 className=" text-[12px] font-semibold font-manrope leading-[16.39px] tracking-[0.01em] gap-8 mb-slide-large text-brown">Subscribe</h4>
-
-//             <div>
-//                 <ul className="flex flex-col h-full space-y-4 mt-4">
-//                     <li className="flex items-center space-x-2 font-manrope font-normal text-[16px] leading-[21.86px] tracking-[0.01em] gap-3">
-//                         <svg className="w-[11px] h-[11px] rounded-[2px] bg-[#C3AD9C] " >
-//                             <use href="#icon-facebook"></use>
-//                         </svg>
-//                         <a href="#" className="text-white">Facebook</a>
-//                     </li>
-//                     <li className="flex items-center space-x-2 font-manrope font-normal text-[16px] leading-[21.86px] tracking-[0.01em] gap-3">
-//                         <svg className="w-[11px] h-[11px] rounded-[2px] bg-[#C3AD9C]">
-//                             <use href="#icon-instagram"></use>
-//                         </svg>
-//                         <a href="#" className="text-white">Instagram</a>
-//                     </li>
-//                 </ul>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default FooterContactList;
-
 const FooterContactList = () => {
   return (
     <div className='h-[400px] bg-black p-[15px] text-white'>
