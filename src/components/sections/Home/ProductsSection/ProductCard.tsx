cd546b0766aabe0d86be07970bc4eed30279fb68
Subx1s0o <nazarsabardin@gmail.com>
2025-01-04 01:21:59 +0200

    style: changing sizes of images in list

--- src/components/sections/Home/ProductsSection/ProductCard.tsx
+++ src/components/sections/Home/ProductsSection/ProductCard.tsx
@@ -27,7 +27,7 @@ export default function ProductCard({ product }: ProductCardProps) {
     : price
 
   return (
-    <li className='aspect-square flex-1 pl-4 sm:flex-1/2 lg:flex-1/4'>
+    <li className='md:flex-1/3 aspect-square flex-1 pl-4 sm:flex-1/2 lg:flex-1/4'>
       <Link href={`catalogue/${category}/${slug}`}>
         <div className='relative mb-3 max-h-[460px] bg-grey-light'>
           <div className='relative aspect-[1]'>
