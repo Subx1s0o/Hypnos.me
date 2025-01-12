0b31cdd0d3e8a2dc17025bca36b958c367657029
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2025-01-12 20:14:01 +0200

    add search fetch

--- src/components/modals/SearchModal/SearchModal.tsx
+++ src/components/modals/SearchModal/SearchModal.tsx
@@ -1,14 +1,12 @@
+'use client'
+
 import React from 'react'
 
-// import { useModal } from '@/components/helpers/ModalContext'
+// import { Product } from '@/types'
 
-import Icon from '@/components/ui/Icon'
-
-// import CartItems from '../CartModal/CartItems'
+import SearchBox from '@/components/modals/SearchModal/SearchBox'
 
 export default function SearchModal() {
-  // const { toggleModal } = useModal()
-
   return (
     <div className='flex flex-col justify-between gap-10 p-4 md:p-8'>
       <div className='flex flex-col items-center'>
@@ -18,21 +16,8 @@ export default function SearchModal() {
           Search
         </h2>
       </div>
-      <search className='relative m-auto flex w-full lg:w-96'>
-        <input
-          className='h-[48px] w-full rounded-[4px] border border-grey-200 bg-white py-4 pl-8 pr-2
-            text-sm outline-none focus-visible:border-black'
-        />
 
-        <Icon
-          className='pointer-events-none absolute left-2 top-1/2 -translate-y-1/2 text-black/60'
-          id='icon-search'
-          w={18}
-          h={18}
-        />
-      </search>
-      {/* викорисаємо верстку знайдених компонентів як і у cart */}
-      {/* {<CartItems />} */}
+      <SearchBox />
     </div>
   )
 }
