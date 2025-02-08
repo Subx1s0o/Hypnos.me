74dd61770749c64cce9dcb68caa97264db7407f0
Subx1s0o <nazarsabardin@gmail.com>
2025-02-09 00:31:54 +0200

    fix: small fixes

--- src/components/ui/BlackBadge.tsx
+++ src/components/ui/BlackBadge.tsx
@@ -7,7 +7,7 @@ interface SpanBtnProps {
 const BlackBadge: React.FC<SpanBtnProps> = ({ children, className = '' }) => {
   return (
     <div
-      className={`flex h-6 items-center justify-center rounded-full bg-black font-manrope text-xs
+      className={`flex items-center justify-center rounded-full bg-black p-6 font-manrope text-xs
         uppercase text-white ${className}`}>
       {children}
     </div>
