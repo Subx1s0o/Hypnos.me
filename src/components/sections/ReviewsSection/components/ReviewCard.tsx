bbc0bf22c3e25a1b07a4520fdf88410aebd48b77
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2025-02-16 12:46:35 +0100

    change svg in sprite for reviews section

--- src/components/sections/ReviewsSection/components/ReviewCard.tsx
+++ src/components/sections/ReviewsSection/components/ReviewCard.tsx
@@ -16,14 +16,14 @@ type ReviewCardProps = {
 export default function ReviewCard({ slide }: ReviewCardProps) {
   return (
     <div className='w-full pr-8'>
-      <div className='mb-8 h-[88px] max-w-[292px] overflow-hidden font-manrope text-base'>
-        <p className='line-clamp-4'>{slide.text}</p>
+      <div className='mb-8 h-[88px] max-w-[292px] overflow-hidden'>
+        <p className='line-clamp-4 text-base'>{slide.text}</p>
       </div>
       <Icon
-        id='icon-reviews-line'
+        id='icon-review'
         w={292}
         h={32}
-        className='fill-transparent stroke-[#D9CABF]'
+        className='text-[#D9CABF]'
       />
       <div className='flex max-w-[200px] items-end gap-6'>
         <Image
@@ -32,7 +32,7 @@ export default function ReviewCard({ slide }: ReviewCardProps) {
           width={64}
           height={64}
         />
-        <div className='flex flex-col gap-1 font-manrope'>
+        <div className='flex flex-col gap-1'>
           <h3 className='text-base leading-[19.2px]'>{slide.author}</h3>
           <p className='text-xs font-semibold text-brown'>{slide.date}</p>
         </div>
--- src/types/sprite.type.ts
+++ src/types/sprite.type.ts
@@ -17,4 +17,4 @@ export type SpriteTypes =
   | 'icon-medal-star'
   | 'icon-diamant'
   | 'icon-email'
-  | 'icon-reviews-line'
+  | 'icon-review'
