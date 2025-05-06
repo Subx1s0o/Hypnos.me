8d374c2a5e16d43ab7862f12cb78889416b4a9a2
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2025-05-06 22:49:54 +0200

    modify not-found page according to Figma

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,51 @@
 'use client'
 
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+
+import Icon from '@/components/ui/Icon'
 
 export default function NotFound() {
   const router = useRouter()
 
   return (
-    <div
-      className='absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col
-        items-center'>
-      <img src='/404.svg' alt='404' />
-      <p className='mb-10 text-center text-xl'>
+    <div className='relative flex flex-col items-center py-[80px]'>
+      <Image
+        src='/images/NotFound/404.avif'
+        alt='404'
+        height={140}
+        width={305}
+        className='mb-6'
+      />
+      <p className='mb-[236px] text-center text-sm md:mb-[100px]'>
         Page not found
       </p>
       <button
-        className='lg:hover:bg-brown-active lg:focus-visible:bg-brown-active rounded-xl bg-brown
-          px-20 py-5 text-smd text-white outline-none transition-colors'
+        className='relative z-30 flex w-[237px] items-center justify-center rounded-[3px] bg-black
+          px-[18px] py-5 text-xs font-extrabold text-white outline-none transition-colors
+          lg:hover:bg-black-hover lg:focus-visible:bg-black-hover'
         onClick={() => router.back()}>
-        Go back
+        <p className=''>Go back</p>
+        <div
+          className='absolute right-4 top-4 flex size-6 rotate-90 items-center justify-center
+            rounded-full bg-white text-black'>
+          <Icon
+            id='icon-arrow'
+            h={14}
+            w={14}
+          />
+        </div>
       </button>
+      <div className='hidden h-[200px] md:block'></div>
+      <div className='absolute bottom-0 hidden max-h-full max-w-full overflow-hidden md:block'>
+        <Image
+          src='/images/NotFound/405.png'
+          width={1701}
+          height={781}
+          alt='404'
+          className=''
+        />
+      </div>
     </div>
   )
 }
